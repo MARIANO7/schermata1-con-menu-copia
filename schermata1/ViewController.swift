32eca4f692860fa227cc5af0291f2a850b6d7d00
Mariano Caiazzo <ilcaiazzo@icloud.com>
2018-09-25 15:49:13 +0200

    Initial Commit

---
+++ schermata1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  schermata1
+//
+//  Created by Mariano Caiazzo on 25/09/18.
+//  Copyright © 2018 iOSFoundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
